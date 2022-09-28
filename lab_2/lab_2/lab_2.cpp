341a505b6b3b6d2ef22f1af5197f2f66d921e4d2
Вова Гнатюк <vololdymyr.gn@gmail.com>
2022-09-28 18:56:09 +0300

    lab_2

---
+++ lab_2/lab_2/lab_2.cpp
@@ -0,0 +1,17 @@
+﻿// Програма, що обчислює площу круга
+
+#include <iostream>
+#include <windows.h>
+using namespace std;
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    float radius, area;
+    cout << "Введіть радіус круга: ";
+    cin >> radius;
+    area = pow(radius, 2) * 3.14;
+    cout << "Площа круга радіусом " << radius << " дорівнює " << area;
+}
